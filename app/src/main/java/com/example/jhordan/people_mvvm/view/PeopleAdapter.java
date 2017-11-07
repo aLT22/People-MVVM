8400547c50b347ff7b19bb0c642f7f902e13b6ce
alexeyturkin <altsworks@gmail.com>
2017-11-07 17:55:24 +0300

    code refactoring (spaces)

--- app/src/main/java/com/example/jhordan/people_mvvm/view/PeopleAdapter.java
+++ app/src/main/java/com/example/jhordan/people_mvvm/view/PeopleAdapter.java
@@ -31,47 +31,50 @@ import java.util.List;
 
 public class PeopleAdapter extends RecyclerView.Adapter<PeopleAdapter.PeopleAdapterViewHolder> {
 
-  private List<People> peopleList;
+    private List<People> peopleList;
 
-  public PeopleAdapter() {
-    this.peopleList = Collections.emptyList();
-  }
-
-  @Override public PeopleAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-    ItemPeopleBinding itemPeopleBinding =
-        DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.item_people,
-            parent, false);
-    return new PeopleAdapterViewHolder(itemPeopleBinding);
-  }
-
-  @Override public void onBindViewHolder(PeopleAdapterViewHolder holder, int position) {
-    holder.bindPeople(peopleList.get(position));
-  }
-
-  @Override public int getItemCount() {
-    return peopleList.size();
-  }
-
-  public void setPeopleList(List<People> peopleList) {
-    this.peopleList = peopleList;
-    notifyDataSetChanged();
-  }
-
-  public static class PeopleAdapterViewHolder extends RecyclerView.ViewHolder {
-    ItemPeopleBinding mItemPeopleBinding;
-
-    public PeopleAdapterViewHolder(ItemPeopleBinding itemPeopleBinding) {
-      super(itemPeopleBinding.itemPeople);
-      this.mItemPeopleBinding = itemPeopleBinding;
+    public PeopleAdapter() {
+        this.peopleList = Collections.emptyList();
     }
 
-    void bindPeople(People people) {
-      if (mItemPeopleBinding.getPeopleViewModel() == null) {
-        mItemPeopleBinding.setPeopleViewModel(
-            new ItemPeopleViewModel(people, itemView.getContext()));
-      } else {
-        mItemPeopleBinding.getPeopleViewModel().setPeople(people);
-      }
+    @Override
+    public PeopleAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        ItemPeopleBinding itemPeopleBinding =
+                DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.item_people,
+                        parent, false);
+        return new PeopleAdapterViewHolder(itemPeopleBinding);
     }
-  }
+
+    @Override
+    public void onBindViewHolder(PeopleAdapterViewHolder holder, int position) {
+        holder.bindPeople(peopleList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return peopleList.size();
+    }
+
+    public void setPeopleList(List<People> peopleList) {
+        this.peopleList = peopleList;
+        notifyDataSetChanged();
+    }
+
+    public static class PeopleAdapterViewHolder extends RecyclerView.ViewHolder {
+        ItemPeopleBinding mItemPeopleBinding;
+
+        public PeopleAdapterViewHolder(ItemPeopleBinding itemPeopleBinding) {
+            super(itemPeopleBinding.itemPeople);
+            this.mItemPeopleBinding = itemPeopleBinding;
+        }
+
+        void bindPeople(People people) {
+            if (mItemPeopleBinding.getPeopleViewModel() == null) {
+                mItemPeopleBinding.setPeopleViewModel(
+                        new ItemPeopleViewModel(people, itemView.getContext()));
+            } else {
+                mItemPeopleBinding.getPeopleViewModel().setPeople(people);
+            }
+        }
+    }
 }
